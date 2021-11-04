f8eaf9b482b4009ca7cc23dd028e102a7cd52891
Eddycodes27 <Eddycodes27@gmail.com>
2021-11-03 20:57:24 -0400

    Updated broken collision

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// Start Game
-// function startscreen () {
-//     ctx.fillStyle = "#000000"
-//     ctx.font = "18px Black Ops One"
-//     ctx.textAlign = "center"
-// }
-
 const game = document.getElementById('canvas')
 
 const ctx = game.getContext('2d')
@@ -12,11 +5,6 @@ const ctx = game.getContext('2d')
 const scoreChange = document.getElementById('score')
 let score = 0
 let collisionCounter = 0
-// startbutton = getElementById('startgame')
-
-// const youWin = document.getElementById('winlose')
-// const win = "Winner"
-// const lose = "Looser"
 
 function Character(x, y, color, width, height) {
     this.x = x
@@ -83,178 +71,123 @@ let bulletShoot = (e) => {
     }
 }
 
-// const enemies = []
-// function respawn() {
-//     if (gameCounter < 3) {
-
-
-//         // for (let i = 0; i < 6; i++) {
-
-            
-
-//             enemies.push(enemy)
-//             enemies.push(enemy2)
-//             enemies.push(enemy3)
-//             enemies.push(enemy4)
-//             enemies.push(enemy5)
-//             enemies.push(enemy6)
-
-
-
-//             const renderEnemy = () => {
-//                 for (let j = 0; j < enemies.length; j++) {
-//                     enemies[j].render()
-//                 }
-//             }
-
-//             const enemyFlying = () => {
-//                 for (i = 0; i < enemies.length; i++) {
-//                     enemies[i].y += 10
-//                 }
-//             }
-
-//         }
-//     }
-
 const explosion = () => {
-    if (enemy.alive = true) {
-        if (
-            player.x < enemy.x + enemy.width &&
-            player.x + player.width > enemy.x &&
-            player.y < enemy.y + enemy.height &&
-            player.y + player.height > enemy.y
-        ) {
-            // kill enemies
-            score += 1000
-            return enemy.alive = false
-            // return scoreChange.innerText = score
-        }  else if (
-            bullet.x < enemy.x + enemy.width &&
-            bullet.x + bullet.width > enemy.x &&
-            bullet.y < enemy.y + enemy.height &&
-            bullet.y + bullet.height > enemy.y
-            ) {   // kill enemies
-            score += 2000
-            return enemy.alive = false
-            }
+    if (
+        player.x < enemy.x + enemy.width &&
+        player.x + player.width > enemy.x &&
+        player.y < enemy.y + enemy.height &&
+        player.y + player.height > enemy.y
+    ) {
+        // kill enemies
+        enemy.alive = false
+        score += 1000
+    } if (
+        bullet.x < enemy.x + enemy.width &&
+        bullet.x + bullet.width > enemy.x &&
+        bullet.y < enemy.y + enemy.height &&
+        bullet.y + bullet.height > enemy.y
+        ) {   // kill enemies
+        enemy.alive = false
+        score += 2000
+        }
+    if (
+        player.x < enemy2.x + enemy2.width &&
+        player.x + player.width > enemy2.x &&
+        player.y < enemy2.y + enemy2.height &&
+        player.y + player.height > enemy2.y
+    ) {        
+        // kill enemies
+        enemy2.alive = false
+        score += 1000
     }
-    
-        // return scoreChange.innerText = score
-    // if (
-    //     player.x < enemy2.x + enemy2.width &&
-    //     player.x + player.width > enemy2.x &&
-    //     player.y < enemy2.y + enemy2.height &&
-    //     player.y + player.height > enemy2.y
-    // ) {        
-    //     // kill enemies
-    //     enemy2.alive = false
-    //     score += 1000
-    //     // score += 1000
-    //     // return scoreChange.innerText = score 
-    // }
-    // if (
-    //     bullet.x < enemy2.x + enemy2.width &&
-    //     bullet.x + bullet.width > enemy2.x &&
-    //     bullet.y < enemy2.y + enemy2.height &&
-    //     bullet.y + bullet.height > enemy2.y
-    //     ) {   // kill enemies
-    //     enemy2.alive = false
-    //     score += 2000
-    //     // return scoreChange.innerText = score
-    //     }
-    // if (
-    //     player.x < enemy3.x + enemy3.width &&
-    //     player.x + player.width > enemy3.x &&
-    //     player.y < enemy3.y + enemy3.height &&
-    //     player.y + player.height > enemy3.y
-    // ) {       
-    //     // kill enemies
-    //     enemy3.alive = false
-    //     score += 1000
-    //     // return scoreChange.innerText = score 
-    // }
-    // if (
-    //     bullet.x < enemy3.x + enemy3.width &&
-    //     bullet.x + bullet.width > enemy3.x &&
-    //     bullet.y < enemy3.y + enemy3.height &&
-    //     bullet.y + bullet.height > enemy3.y
-    //     ) {   // kill enemies
-    //     enemy3.alive = false
-    //     score += 2000
-    //     // return scoreChange.innerText = score
-    //     }
-    // if (
-    //     player.x < enemy4.x + enemy4.width &&
-    //     player.x + player.width > enemy4.x &&
-    //     player.y < enemy4.y + enemy4.height &&
-    //     player.y + player.height > enemy4.y
-    // ) {        
-    //     // kill enemies
-    //     enemy4.alive = false
-    //     score += 1000
-    //     // return scoreChange.innerText = score 
-    // } 
-    // if (
-    //     bullet.x < enemy4.x + enemy4.width &&
-    //     bullet.x + bullet.width > enemy4.x &&
-    //     bullet.y < enemy4.y + enemy4.height &&
-    //     bullet.y + bullet.height > enemy4.y
-    //     ) {   // kill enemies
-    //     enemy4.alive = false
-    //     score += 2000
-    //     // return scoreChange.innerText = score
-    //     }
-    // if (
-    //     player.x < enemy5.x + enemy5.width &&
-    //     player.x + player.width > enemy5.x &&
-    //     player.y < enemy5.y + enemy5.height &&
-    //     player.y + player.height > enemy5.y
-    // ) {        
-    //     // kill enemies
-    //     enemy5.alive = false
-    //     score += 1000
-    //     // return scoreChange.innerText = score 
-    // }
-    // if (
-    //     bullet.x < enemy5.x + enemy5.width &&
-    //     bullet.x + bullet.width > enemy5.x &&
-    //     bullet.y < enemy5.y + enemy5.height &&
-    //     bullet.y + bullet.height > enemy5.y
-    //     ) {   // kill enemies
-    //     enemy5.alive = false
-    //     score += 2000
-    //     // return scoreChange.innerText = score
-    //     }
-    // if (
-    //     player.x < enemy6.x + enemy6.width &&
-    //     player.x + player.width > enemy6.x &&
-    //     player.y < enemy6.y + enemy6.height &&
-    //     player.y + player.height > enemy6.y
-    // ) {        
-    //     // kill enemies
-    //     enemy6.alive = false
-    //     score += 1000
-    //     // return scoreChange.innerText = score 
-    // }
-    // if (
-    //     bullet.x < enemy6.x + enemy6.width &&
-    //     bullet.x + bullet.width > enemy6.x &&
-    //     bullet.y < enemy6.y + enemy6.height &&
-    //     bullet.y + bullet.height > enemy6.y
-    //     ) {   
-    //         // kill enemies
-    //     enemy6.alive = false
-    //     score += 2000
-        
-        // 
-    // }
-    // if (
-    //     enemy.alive + enemy2.alive === false
-    // ) {
-    //     return win.innerText = winlose
-    // }
+    if (
+        bullet.x < enemy2.x + enemy2.width &&
+        bullet.x + bullet.width > enemy2.x &&
+        bullet.y < enemy2.y + enemy2.height &&
+        bullet.y + bullet.height > enemy2.y
+        ) {   // kill enemies
+        enemy2.alive = false
+        score += 2000
+        }
+    if (
+        player.x < enemy3.x + enemy3.width &&
+        player.x + player.width > enemy3.x &&
+        player.y < enemy3.y + enemy3.height &&
+        player.y + player.height > enemy3.y
+    ) {       
+        // kill enemies
+        enemy3.alive = false
+        score += 1000 
+    }
+    if (
+        bullet.x < enemy3.x + enemy3.width &&
+        bullet.x + bullet.width > enemy3.x &&
+        bullet.y < enemy3.y + enemy3.height &&
+        bullet.y + bullet.height > enemy3.y
+        ) {   // kill enemies
+        enemy3.alive = false
+        score += 2000
+        }
+    if (
+        player.x < enemy4.x + enemy4.width &&
+        player.x + player.width > enemy4.x &&
+        player.y < enemy4.y + enemy4.height &&
+        player.y + player.height > enemy4.y
+    ) {        
+        // kill enemies
+        enemy4.alive = false
+        score += 1000
+    } 
+    if (
+        bullet.x < enemy4.x + enemy4.width &&
+        bullet.x + bullet.width > enemy4.x &&
+        bullet.y < enemy4.y + enemy4.height &&
+        bullet.y + bullet.height > enemy4.y
+        ) {   // kill enemies
+        enemy4.alive = false
+        score += 2000
+        }
+    if (
+        player.x < enemy5.x + enemy5.width &&
+        player.x + player.width > enemy5.x &&
+        player.y < enemy5.y + enemy5.height &&
+        player.y + player.height > enemy5.y
+    ) {        
+        // kill enemies
+        enemy5.alive = false
+        score += 1000
+    }
+    if (
+        bullet.x < enemy5.x + enemy5.width &&
+        bullet.x + bullet.width > enemy5.x &&
+        bullet.y < enemy5.y + enemy5.height &&
+        bullet.y + bullet.height > enemy5.y
+        ) {   // kill enemies
+        enemy5.alive = false
+        score += 2000
+        }
+    if (
+        player.x < enemy6.x + enemy6.width &&
+        player.x + player.width > enemy6.x &&
+        player.y < enemy6.y + enemy6.height &&
+        player.y + player.height > enemy6.y
+    ) {        
+        // kill enemies
+        enemy6.alive = false
+        score = 1000 
+    }
+    if (
+        bullet.x < enemy6.x + enemy6.width &&
+        bullet.x + bullet.width > enemy6.x &&
+        bullet.y < enemy6.y + enemy6.height &&
+        bullet.y + bullet.height > enemy6.y
+        ) {   
+            // kill enemies
+        enemy6.alive = false
+        score += 2000
 }
 
+}
 
 const gameLoop = () => {
     // clear the canvas
@@ -298,6 +231,7 @@ const gameLoop = () => {
     }
     // render player and bullets
     // respawn()
+    explosion()
     player.render()
     
     // bullet.render()
@@ -314,4 +248,3 @@ document.addEventListener('keyup', bulletShoot)
 
 // the timing function will determine how and when our game animates
 let gameInterval = setInterval(gameLoop, 50)
-explosion()
